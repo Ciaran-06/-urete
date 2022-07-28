1220c944893d06e2bc89b711e97d77d413994fb4
Ciaran-06 <76187734+Ciaran-06@users.noreply.github.com>
2022-07-28 14:16:08 +0100

    adding styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className='container'>
+      <div className='header'>
+        <h1 id="header-banner-text">うれて</h1>
+      </div>
       {Array(4).fill(<Card />)}
     </div>
   );
@@ -14,7 +17,7 @@ class Card extends React.Component {
   constructor(props) {
     super(props);
     {
-      this.state = {Address: "Edinbrugh", Price: "£1000", Description: "loveley family home"}
+      this.state = {Address: "Edinbrugh", Image: "https://imgur.com/a/UkaAz1f", Price: "£1000", Description: "loveley family home"}
     }
   }
 
@@ -22,7 +25,7 @@ class Card extends React.Component {
     return (
       <div className='card-container'>
         <h2 className='card-address'>{this.state.Address}</h2>
-        <img className='card-image'>{this.state.Image}</img>
+        <img className='card-image' src={this.state.Image} alt="alt"></img>
         <h4 className='card-price'>{this.state.Price}</h4>
         <p className='card-description'>{this.state.Description}</p>
       </div>
